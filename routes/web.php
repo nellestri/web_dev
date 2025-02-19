d4121ffd68cfc15c81f31f1fa7b28aad857e3023
Nell <nell@Nells-MacBook-Pro.local>
2025-02-19 12:27:32 +0800

    syste chnges

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
 });
+Route::get('/students-list',function () {
+    return view('based');
+});
+Route::get('/students-list', [StudentsController::class, 'index']);
